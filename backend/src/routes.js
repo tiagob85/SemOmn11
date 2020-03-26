cc5b72a258c213738abdaa494f8080fe6705caa8
tiagob85 <tiago.gpes@gmail.com>
2020-03-26 16:05:59 -0300

    Criação dos controladores para perfil e sessão, alteração das rotas conforme adição dos controllers, paginação da listagem de casos, contador de casos, implementação do cors, listagem com multiplas tabelas utilizando join

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,21 @@ const crypto = require('crypto');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+/**
+ * Sessions
+ */
+routes.post('/sessions', SessionController.create);
+
+/**
+ * Controllers
+ */
+routes.get('/profile', ProfileController.index);
+
 /**
  * Ongs
  */
@@ -17,5 +30,7 @@ routes.get('/ongs', OngController.index);
 
  routes.post('/incidents', IncidentController.create);
  routes.get('/incidents', IncidentController.index);
+ routes.delete('/incidents/:id', IncidentController.delete);
+ 
 
 module.exports = routes;
